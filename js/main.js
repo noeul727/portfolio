4c7814b7f2cb969e5837987c650e88d2494a69d9
noeul <164986888+noeul727@users.noreply.github.com>
2025-08-27 14:59:51 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  var page = $('.fullpage').fullpage({
+    
+    navigation : true,
+    navigationTooltips: ['home','contents', 'introduction', 'personal project', 'team project','BX Design'],
+    showActiveTooltip: false, // false로 설정 (우리가 직접 만들 거라서)
+    navigationPosition: 'left',
+
+    //1024이하 동작 x
+    responsiveWidth: 1024,
+
+    //마지막 영역 #fp-nav 숨기기
+    onLeave : function(index, nextIndex, direction){
+      if(nextIndex == 6){
+        $("#fp-nav").fadeOut();
+      }else{
+        $("#fp-nav").fadeIn();
+      }
+    },
+
+    // fullPage 초기화 후 실행
+    afterRender: function(){
+      // 각 네비게이션에 텍스트 직접 추가
+      const tooltips = ['home','contents', 'introduction', 'personal project', 'team project','BX Design'];
+      
+      $('#fp-nav ul li').each(function(index){
+        $(this).append('<span class="custom-tooltip">' + tooltips[index] + '</span>');
+      });
+    }
+  });
+  
+});
